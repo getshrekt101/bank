47eca6d6ac0d5da780baddc6a64c4b3bc216dcd5
Steve Rhodes <stevie912@gmail.com>
2025-04-02 19:26:47 -0400

    HTML + CSS pages and controller for UI

--- src/main/java/com/algomau/bank/BankApplication.java
+++ src/main/java/com/algomau/bank/BankApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableJpaAuditing
 public class BankApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(BankApplication.class, args);
     }
 
